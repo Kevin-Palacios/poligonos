2826ec1334d71f29b65f3ed3cf296b0168d18f97
unknown <kapemonito14@homail.com>
2022-06-04 14:30:33 -0500

    Ya hay figuras muestras

--- graficar.py
+++ graficar.py
@@ -26,44 +26,31 @@ remove(ruta)
 file = open(ruta, "w")
 file.write("x y\n")
 file.close()
+n=0
+op=0
+print("Hola, quieres ingresar los puntos (1) o elegir una figura predeterminada (2)?")
+op=int(input())
+if(op==2):
+    opcion=6
+else:
+    
 
-print("Hola, ingresa la cantidad de puntos que quieras agregar")
-#Cantidad de lados
-n=int(input())
+    print("Hola, ingresa la cantidad de puntos que quieras agregar")
+    #Cantidad de lados
+    n=int(input())
 
-matriz = [[0] * 2 for i in range(n)]
-
-#Ciclo para obtener las coordenadas
-for i in range(0, n):
-    print("Ingresa x del punto "+str(i+1))
-    #coordenada en x
-    x=float(input())
-    matriz[i][0]=x
-    print("Ingresa y del punto "+str(i+1))
-    #coordenada en y
-    y=float(input())
-    matriz[i][1]=y
-    #cadena para agregar a un archivo de texto del cual se leerá la coordenada
-    cadena=str(x)+" "+str(y)
-    #se abre el archivo en a para append(añadir)
-    with open(ruta, 'a') as f:
-        #se inserta la cadena al archivo
-        f.write(cadena+"\r")
-        f.close()
-
-print(matriz)
-
-def actualizarCoordenadas(matriz, ruta):
-    file = open(ruta, "w")
-    file.write("x y\n")
-    file.close()
+    matriz = [[0] * 2 for i in range(n)]
 
     #Ciclo para obtener las coordenadas
     for i in range(0, n):
+        print("Ingresa x del punto "+str(i+1))
         #coordenada en x
-        x=matriz[i][0]
+        x=float(input())
+        matriz[i][0]=x
+        print("Ingresa y del punto "+str(i+1))
         #coordenada en y
-        y= matriz[i][1]
+        y=float(input())
+        matriz[i][1]=y
         #cadena para agregar a un archivo de texto del cual se leerá la coordenada
         cadena=str(x)+" "+str(y)
         #se abre el archivo en a para append(añadir)
@@ -72,8 +59,11 @@ def actualizarCoordenadas(matriz, ruta):
             f.write(cadena+"\r")
             f.close()
 
+    print(matriz)
 
-def graficar(contador):
+
+
+def graficar(contador, ruta):
     # Cargamos el csv
     data=pd.read_csv(ruta ,header=0,delim_whitespace=True)
 
@@ -129,6 +119,26 @@ def graficar(contador):
     plt.show()
     #return nombreGrafica
 
+def actualizarCoordenadas(matriz, ruta, n):
+    file = open(ruta, "w")
+    file.write("x y\n")
+    file.close()
+
+    #Ciclo para obtener las coordenadas
+    for i in range(0, n):
+        #coordenada en x
+        x=matriz[i][0]
+        #coordenada en y
+        y= matriz[i][1]
+        #cadena para agregar a un archivo de texto del cual se leerá la coordenada
+        cadena=str(x)+" "+str(y)
+        #se abre el archivo en a para append(añadir)
+        with open(ruta, 'a') as f:
+            #se inserta la cadena al archivo
+            f.write(cadena+"\r")
+            f.close()
+
+
 def multiplicarMatrices(matriz, matriz2):
 
     multiplicacion = [[0] * 2 for i in range(len(matriz))]
@@ -160,7 +170,7 @@ def reflexion(matriz, eje, ruta):
         print(transformacion)
         matriz=multiplicarMatrices(matriz, transformacion)
     
-    actualizarCoordenadas(matriz, ruta)
+    actualizarCoordenadas(matriz, ruta, n)
     return matriz
 
 def expandir(matriz, c, eje, ruta):
@@ -182,7 +192,7 @@ def expandir(matriz, c, eje, ruta):
         print(transformacion)
         matriz=multiplicarMatrices(matriz, transformacion)
     
-    actualizarCoordenadas(matriz, ruta)
+    actualizarCoordenadas(matriz, ruta, n)
     return matriz
 
 def cortar(matriz, c, eje, ruta):
@@ -204,7 +214,7 @@ def cortar(matriz, c, eje, ruta):
         print(transformacion)
         matriz=multiplicarMatrices(matriz, transformacion)
     
-    actualizarCoordenadas(matriz, ruta)
+    actualizarCoordenadas(matriz, ruta, n)
     return matriz
 
 def rotar(matriz, c, eje, ruta):
@@ -218,24 +228,150 @@ def rotar(matriz, c, eje, ruta):
     matriz=multiplicarMatrices(matriz, transformacion)
     print(matriz)
     
-    actualizarCoordenadas(matriz, ruta)
+    actualizarCoordenadas(matriz, ruta, n)
     return matriz
+
+ruta1="Programa poligonos/poligono1.txt"
+file = open(ruta1, "w")
+file.write("x y\n")
+file.close()
+with open(ruta1, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("2 2"+"\r")
+    f.write("2 -2"+"\r")
+    f.write("-2 2"+"\r")
+    f.write("-2 -2"+"\r")
+    f.close()
+
+ruta2="Programa poligonos/poligono2.txt"
+file = open(ruta2, "w")
+file.write("x y\n")
+file.close()
+with open(ruta2, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("0 1"+"\r")
+    f.write("3 3"+"\r")
+    f.write("-3 3"+"\r")
+    f.write("-2 4"+"\r")
+    f.write("2 4"+"\r")
+    f.write("0 3.5"+"\r")
+    f.close()
+
+
+ruta3="Programa poligonos/poligono3.txt"
+file = open(ruta3, "w")
+file.write("x y\n")
+file.close()
+with open(ruta3, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("0 9"+"\r")
+    f.write("2 2"+"\r")
+    f.write("-8 2"+"\r")
+    f.write("8 2"+"\r")
+    f.write("-2 2"+"\r")
+    f.write("3 -2"+"\r")
+    f.write("-3 -2"+"\r")
+    f.write("0 -5"+"\r")
+    f.write("5 -9"+"\r")
+    f.write("-5 -9"+"\r")
+
+    f.close()
+
+
+ruta4="Programa poligonos/poligono4.txt"
+file = open(ruta4, "w")
+file.write("x y\n")
+file.close()
+with open(ruta4, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("0 0"+"\r")
+    f.write("0 8"+"\r")
+    f.write("6 8"+"\r")
+    f.write("6 6"+"\r")
+    f.write("2 6"+"\r")
+    f.write("2 4"+"\r")
+    f.write("4 4"+"\r")
+    f.write("4 2"+"\r")
+    f.write("2 2"+"\r")
+    f.write("2 0"+"\r")
+
+    f.close()
+
+ruta5="Programa poligonos/poligono5.txt"
+file = open(ruta5, "w")
+file.write("x y\n")
+file.close()
+with open(ruta5, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("6 7"+"\r")
+    f.write("2 7"+"\r")
+    f.write("1 8"+"\r")
+    
+    f.close()
+
+
+"""ruta5="Programa poligonos/poligono5.txt"
+file = open(ruta5, "w")
+file.write("x y\n")
+file.close()
+with open(ruta5, 'a') as f:
+    #se inserta la cadena al archivo
+    f.write("-10 1"+"\r")
+    f.write("-9 2"+"\r")
+    f.write("-7 3"+"\r")
+    f.write("-5 3"+"\r")
+    f.write("-3 6"+"\r")
+    f.write("0 4"+"\r")
+    f.write("4 3"+"\r")
+    f.write("6 1"+"\r")
+    f.write("8 7"+"\r")
+    f.write("11 0"+"\r")
+    f.write("12 -2"+"\r")
+    f.write("10 -1"+"\r")
+    f.write("6 -3"+"\r")
+    f.write("5 -5"+"\r")
+    f.write("2 -4"+"\r")
+    f.write("-5 -4"+"\r")
+    f.write("-7 -2"+"\r")
+    f.write("-7 -6"+"\r")
+    f.write("-8 -4"+"\r")
+    f.write("-9 -2"+"\r")
+    f.write("-9 -1"+"\r")
+    f.write("-7 0"+"\r")
+    
+
+    f.close()
+"""
+#call it what you want
+
 
 
 opcion=0
 contador=0
 Graficas=[]
-Graficas.append(graficar(contador))
+if(op!=2):
+    Graficas.append(graficar(contador, ruta))
 contador=int(contador)+1
-while(opcion!=6):
-    print("Ingresa lo que quieras hacer: ")
-    print("1-Reflectar")
-    print("2-Expandir")
-    print("3-Cortar")
-    print("4-Rotar con respecto al origen")
-    print("5-Graficar")
-    print("6-Salir")
-    opcion=int(input())
+opcion2=0
+ej1="ej1"
+ej2="ej2"
+ej3="ej3"
+ej4="ej4"
+ej5="ej5"
+while(opcion!=7):
+    if(op!=2):
+        print("Ingresa lo que quieras hacer: ")
+        print("1-Reflectar")
+        print("2-Expandir")
+        print("3-Cortar")
+        print("4-Rotar con respecto al origen")
+        print("5-Graficar")
+        print("6-Mostrar ejemplos predeterminados")
+        print("7-Salir")
+        opcion=int(input())
+    else:
+        opcion=6
+        op=1
     eje=""
     v=True
     c=0
@@ -247,7 +383,7 @@ while(opcion!=6):
                 print("Ingresa el eje: ")
                 eje=input()
         matriz=reflexion(matriz, eje, ruta)
-        Graficas.append(graficar(contador))
+        Graficas.append(graficar(contador, ruta))
         contador=int(contador)+1
     elif(opcion==2):
         while(v):
@@ -258,8 +394,10 @@ while(opcion!=6):
                 eje=input()
         print("Ingresa el valor de la constante para expandir")
         c=float(input())
+        aux=c
+        print("La constante es: "+str(aux))
         matriz=expandir(matriz, c, eje, ruta)
-        Graficas.append(graficar(contador))
+        Graficas.append(graficar(contador, ruta))
         contador=int(contador)+1
     elif(opcion==3):
         while(v):
@@ -271,18 +409,129 @@ while(opcion!=6):
         print("Ingresa el valor de la constante para cortar")
         c=float(input())
         matriz=cortar(matriz, c, eje, ruta)
-        Graficas.append(graficar(contador))
+        Graficas.append(graficar(contador, ruta))
         contador=int(contador)+1
     elif(opcion==4):
         print("Ingresa el valor del ángulo para rotar")
         c=float(input())
         matriz=rotar(matriz, c, eje, ruta)
-        Graficas.append(graficar(contador))
+        Graficas.append(graficar(contador, ruta))
         contador=int(contador)+1
     elif(opcion==5):
-        Graficas.append(graficar(contador))
+        Graficas.append(graficar(contador, ruta))
         contador=int(contador)+1
     elif(opcion==6):
+        opcion=0
+        print("Opcion 1")
+        print("Opcion 2")
+        print("Opcion 3")
+        print("Opcion 4")
+        print("Opcion 5")
+        opcion2=int(input())
+        if(opcion2==1):
+            graficar(ej1, ruta1)
+            
+            
+
+            with open(ruta1) as coordenadas:
+                listaAux = coordenadas.readlines()
+            coordenadas.close()
+            print(listaAux)
+            for i in range(len(listaAux)):
+                listaAux[i]=listaAux[i].rstrip("\n")
+            #print(listaAux)
+            n=len(listaAux)-1
+            matriz = [[0] * 2 for i in range(n)]
+            for i in range(len(matriz)):
+                matriz[i]=listaAux[i+1].split(" ")
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    matriz[i][j]=float(matriz[i][j])
+            print(len(listaAux))
+            actualizarCoordenadas(matriz, ruta, n)
+
+        elif(opcion2==2):
+            graficar(ej2, ruta2)
+
+            with open(ruta2) as coordenadas:
+                listaAux = coordenadas.readlines()
+            coordenadas.close()
+            print(listaAux)
+            for i in range(len(listaAux)):
+                listaAux[i]=listaAux[i].rstrip("\n")
+            #print(listaAux)
+            n=len(listaAux)-1
+            matriz = [[0] * 2 for i in range(n)]
+            for i in range(len(matriz)):
+                matriz[i]=listaAux[i+1].split(" ")
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    matriz[i][j]=float(matriz[i][j])
+            print(len(listaAux))
+            actualizarCoordenadas(matriz, ruta, n)
+        elif(opcion2==3):
+            graficar(ej3, ruta3)
+
+            with open(ruta3) as coordenadas:
+                listaAux = coordenadas.readlines()
+            coordenadas.close()
+            print(listaAux)
+            for i in range(len(listaAux)):
+                listaAux[i]=listaAux[i].rstrip("\n")
+            #print(listaAux)
+            n=len(listaAux)-1
+            matriz = [[0] * 2 for i in range(n)]
+            for i in range(len(matriz)):
+                matriz[i]=listaAux[i+1].split(" ")
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    matriz[i][j]=float(matriz[i][j])
+            print(len(listaAux))
+            actualizarCoordenadas(matriz, ruta, n)
+
+        elif(opcion2==4):
+            graficar(ej4, ruta4)
+
+            with open(ruta4) as coordenadas:
+                listaAux = coordenadas.readlines()
+            coordenadas.close()
+            print(listaAux)
+            for i in range(len(listaAux)):
+                listaAux[i]=listaAux[i].rstrip("\n")
+            #print(listaAux)
+            n=len(listaAux)-1
+            matriz = [[0] * 2 for i in range(n)]
+            for i in range(len(matriz)):
+                matriz[i]=listaAux[i+1].split(" ")
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    matriz[i][j]=float(matriz[i][j])
+            print(len(listaAux))
+            actualizarCoordenadas(matriz, ruta, n)
+
+        elif(opcion2==5):
+            graficar(ej5, ruta5)
+
+            with open(ruta5) as coordenadas:
+                listaAux = coordenadas.readlines()
+            coordenadas.close()
+            print(listaAux)
+            for i in range(len(listaAux)):
+                listaAux[i]=listaAux[i].rstrip("\n")
+            #print(listaAux)
+            n=len(listaAux)-1
+            matriz = [[0] * 2 for i in range(n)]
+            for i in range(len(matriz)):
+                matriz[i]=listaAux[i+1].split(" ")
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    matriz[i][j]=float(matriz[i][j])
+            print(len(listaAux))
+            actualizarCoordenadas(matriz, ruta, n)
+        else:
+            print("opcion no valida")
+        
+    elif(opcion==7):
         print("Bai")
     else:
         print("Opcion invalida")
